566786522c3f1e554edae466dedc92348d5e79af
Akond Rahman <akond.rahman.buet@gmail.com>
2016-11-30 10:56:11 -0500

    funda practice

---
+++ funda_/graphSearch.py
@@ -0,0 +1,11 @@
+'''
+Author: Akond Nov 30, 2016
+'''
+from collections import deque
+theGraph={'s':['a', 'b', 'c'],
+          'a':['d'],
+          'b':['e'],
+          'c':['f'],
+          'g':[]
+}
+#queue.popleft()
