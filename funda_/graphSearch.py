ab72ba820071d8ab996d5df98a39072a40d539fe
Akond Rahman <akond.rahman.buet@gmail.com>
2016-11-30 14:44:23 -0500

    bfs

--- funda_/graphSearch.py
+++ funda_/graphSearch.py
@@ -1,11 +1,35 @@
 '''
 Author: Akond Nov 30, 2016
 '''
-from collections import deque
+def performBFS(theGraphParam, rootParam):
+  outputBFS={}
+  currentTraversedPath=[]
+  nodeToTraverse = rootParam
+  theQueue = [rootParam]
+  outputBFS[rootParam] = ('NULL', 0, [])
+  while (len(theQueue) > 0):
+   #print "before:", theQueue
+   nodeToTraverse = theQueue.pop(0)
+   #print "after:", theQueue
+   #currentTraversedPath.append(nodeToTraverse)
+   if nodeToTraverse in theGraphParam:
+     adjacencyList = theGraphParam[nodeToTraverse]
+     for node_ in adjacencyList:
+       if node_ not in theQueue:
+         theQueue.append(node_)
+         currentDistance = outputBFS[nodeToTraverse][1] + 1
+         currPath = outputBFS[nodeToTraverse][2] + [nodeToTraverse]
+         outputBFS[node_] = (nodeToTraverse, currentDistance, currPath)
+  return outputBFS
+
+
 theGraph={'s':['a', 'b', 'c'],
           'a':['d'],
           'b':['e'],
           'c':['f'],
+          'd':['g'],
+          'e':['g'],
           'g':[]
 }
-#queue.popleft()
+bfs_output = performBFS(theGraph, 's')
+print bfs_output
