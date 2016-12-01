36bcff62e4922c579bd5bcf191f87008371eb599
Akond Rahman <akond.rahman.buet@gmail.com>
2016-11-30 20:18:42 -0500

    funda dfs

--- funda_/graphSearch.py
+++ funda_/graphSearch.py
@@ -22,6 +22,34 @@ def performBFS(theGraphParam, rootParam):
          outputBFS[node_] = (nodeToTraverse, currentDistance, currPath)
   return outputBFS
 
+def performDFS(graphParam, rootParam):
+  stackToTrack = []
+  outputDFS = []
+  nodeToTraverse=''
+  popFlag=False
+  stackToTrack.append(rootParam)
+  while(len(stackToTrack) > 0):
+   print "lol:", stackToTrack
+   nodeToTraverse = stackToTrack[len(stackToTrack)-1]
+   if (nodeToTraverse in graphParam):
+     adj_nodes = graphParam[nodeToTraverse]
+     if ((nodeToTraverse not in outputDFS) and (len(adj_nodes) > 0 )):
+       for adj_node in adj_nodes:
+          if ((adj_node not in stackToTrack) and (len(adj_node) > 0) and (adj_node not in outputDFS)):
+            stackToTrack.append(adj_node)
+          else:
+            popFlag = True
+     else:
+            popFlag = True
+   else:
+            popFlag = True
+   if popFlag:
+     outputDFS.append( stackToTrack.pop())
+     popFlag=False
+  #print "elem:", elemToPop
+  return outputDFS
+
+
 
 theGraph={'s':['a', 'b', 'c'],
           'a':['d'],
@@ -32,4 +60,6 @@ theGraph={'s':['a', 'b', 'c'],
           'g':[]
 }
 bfs_output = performBFS(theGraph, 's')
-print bfs_output
+#print bfs_output
+dfs_output = performDFS(theGraph, 's')
+print dfs_output
