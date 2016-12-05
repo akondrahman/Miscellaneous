da76474cd9daebd3242ed206fe063a84ca47edc2
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-04 19:43:46 -0500

    saving

--- funda_/OnSort.py
+++ funda_/OnSort.py
@@ -21,6 +21,29 @@ def performRadixSort(inpList):
          updated_ = tmp_ + [elem_]
          newDict[indexToPut] = updated_
     return newDict
-input=[23, 1, 2, 11, 7, 50, 19]
-answer = performRadixSort(input)
-print answer
+
+def performCountingSort(inpParam):
+    m = max(inpParam) + 1
+    temp_holder=[0]*m
+    output = []
+    print "orig:{}, temp:{}".format(len(inpParam), len(temp_holder))
+    for elem in inpParam:
+        temp_holder[elem] = temp_holder[elem] + 1
+    total= 0
+    for ind_ in xrange(len(temp_holder)):
+        oldCount = temp_holder[ind_]
+        total = total + oldCount
+        temp_holder[ind_] = total
+    k = len(inpParam) - 1
+    output = [0]*m
+    while k >= 0:
+        elem_ = inpParam[k]
+        indexToPut =  temp_holder[elem_]
+        output[indexToPut] = elem_
+        k = k - 1
+    return output
+input_=[23, 1, 2, 11, 7, 50, 19]
+radix_answer = performRadixSort(input_)
+#print radix_answer
+counting_answer = performCountingSort(input_)
+print counting_answer
