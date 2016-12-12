1ff59ec82e9149bd963ebe8192a6ba25cf74362e
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-12 18:34:40 -0500

    goog prac

--- funda_/googlePrac.py
+++ funda_/googlePrac.py
@@ -10,5 +10,23 @@ def googleProblemOne(dictParam):
         print strToPrint
         strToPrint=''
 
+
+
+def googleProblemTwo(stackList, number):
+   tracker = []
+   checker_ = 0
+   while checker_  <= number:
+     if checker_==number:
+         break
+     else:
+       for stack in stackList:
+         popElem = stack.pop()
+         tracker = tracker + [popElem]
+         checker_ =  sum(tracker)
+   return tracker
 employeeDict={'AAA':['BBB', 'CCC', 'EEE'], 'CCC':['DDD']}
-googleProblemOne(employeeDict)
+#googleProblemOne(employeeDict)
+number_ = 6
+lol= [[1, 2], [3, 4]]
+out_ = googleProblemTwo(lol, number_)
+print out_
