716d2f075bdbb7df4898d2899ba3a650b8ac2005
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-14 12:05:45 -0500

    goog day 3

--- funda_/GooglePracDayThree.py
+++ funda_/GooglePracDayThree.py
@@ -10,5 +10,14 @@ def GoogleProbOne(arrayP):
         sumOfSub=sum(subArray)
         if ((sumOfSub>=i_) and (sumOfSub<=j_)):
            print "Eligible sub-array:",subArray
+
+
+def GoogleProbTwo(arrayP):
+   for x_ in xrange(len(arrayP)):
+    fHalf = arrayP[0:x_+1]
+    lHalf = arrayP[x_+1:]
+    if (sum(fHalf)==sum(lHalf)):
+        print "The index is:", x_
+
 arra_=[5, 1, 3, 4, 7, 6, 1, 2]
 GoogleProbOne(arra_)
