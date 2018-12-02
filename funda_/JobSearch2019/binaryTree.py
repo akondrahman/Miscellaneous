df85495dbd319009454e7043610cdfb3ee5deea6
Akond Rahman <akond.rahman.buet@gmail.com>
2018-12-02 18:34:44 -0500

    binary tree

--- funda_/JobSearch2019/binaryTree.py
+++ funda_/JobSearch2019/binaryTree.py
@@ -9,11 +9,34 @@ class node:
         self.right = None 
         self.data = data_ 
     
-    def printTree():
-        print self.data 
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print self.data
+        if self.right:
+            self.right.printTree()
+
+    def insertData(self, data_):
+        if self.data: 
+           if data_ < self.data: 
+               if self.left is None: 
+                   self.left = node(data_)
+               else:
+                   self.left.insertData(data_)
+           elif data_ > self.data : 
+               if self.right is None:
+                   self.right = node(data_)
+               else:
+                   self.right.insertData(data_)
+        else: 
+           self.data = data_
+    
     
 
 if __name__=='__main__':
     root = node(99)
+    root.insertData(6)
+    root.insertData(14)
+    root.insertData(3)
     root.printTree()
 
